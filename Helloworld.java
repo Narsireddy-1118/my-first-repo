228f1f5103c5be6afe81ae212a178efe59962375
Narsireddy-1118 <narsireddy225b@gmail.com>
2025-02-03 15:08:09 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Helo, Jenkins and SonarQube!");
+        System.out.println("Helo, Jen and SonarQube!");
     }
 }
